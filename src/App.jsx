cfb665d5f75998b2dd6a295b039459fc910a56b5
Sima kumari <simak4484@gmail.com>
2026-01-18 23:55:48 +0530

    Day 3: React practice props

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
+import React from "react";
+import User from "./User.jsx";
 function App(){
-  function callFun(){
-    alert("function called")
-  }
-//arrow function bhi use kr skte h
-const fruit =() => {
-  alert("fruit function called")
-}
-
+  //let name="sima";
   return(
     <div>
       <h1>Event and function call</h1>
-      <button className="" onClick={callFun}>Click Me</button>
-      <button className="" onClick={fruit}>Click Me</button>
-      {/*{fruit} ke place pe {callFun} bhi use kr skte h
-      if we used to {callFun()} or {fruit()} here it will call the function on every render that means automatically call hone lagega without clicking the button, esliye hm use krenge {callFun} or {fruit}*/}
+     {/* <h2>hello {name}</h2> */}
+       <User name="sima" age={20}/>
     </div>
   )
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,4 +1,10 @@
-function User(){
-    return <h1>User Component</h1>;
+function User(props){
+   console.log(props);//props object ke place pe kuchh bhi pass kr skte h
+
+    return (
+    <div>
+        <h1>User Component</h1>
+        </div>
+    );
 }
 export default User;
